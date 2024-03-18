7c730d905fd0d268c8e01850736517e6ad4c9cc4
이건창 <geonc123@estsoft.com>
2024-03-18 15:28:44 +0900

    init : 데이터 세팅

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello! intellij dev container
+
+인텔리제이의 dev container 가 beta 서비스를 시작했습니다.
+어떤 기능인지 잘 모르겠더라구요. 그래서 직접 실행해봤습니다.
+
---
+++ src/main/kotlin/tis/hello/HelloController.kt
@@ -0,0 +1,12 @@
+package tis.hello
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello, world!"
+    }
+}
