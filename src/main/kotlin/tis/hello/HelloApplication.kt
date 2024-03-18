36def58b3d4431fb148b97eac3e1bc88b30280c9
이건창 <geonc123@estsoft.com>
2024-03-18 15:29:55 +0900

    init : 환경 세팅

---
+++ src/main/kotlin/tis/hello/HelloApplication.kt
@@ -0,0 +1,11 @@
+package tis.hello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloApplication>(*args)
+}
